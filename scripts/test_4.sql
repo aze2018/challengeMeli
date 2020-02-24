6785aa901943ed6be29d8e92445ee538a085037c
agustin zeppa <aze2020@debian.agus.me>
2020-02-24 02:17:38 -0300

    actualizacion script 4

--- scripts/test_4.sql
+++ scripts/test_4.sql
@@ -1,1 +1,1 @@
-select Server_ID, IP, Exec_date, (select count(*) from PROCESSES_PER_EXECUTION PPE  where PPE.Execution_ID = E.Exec_ID) as 'Cantidad de Procesos Ejecutando' from Executions E join Servers S on E.Server_ID = S.Server_ID ;
+select S.Server_ID, S.IP, E.Exec_date, (select count(*) from PROCESSES_PER_EXECUTION PPE where PPE.Execution_ID = E.Exec_ID) as 'Cantidad de Procesos Ejecutando' from Executions E join Servers S on E.Server_ID = S.Server_ID ;
